075ccd92105567ab0c2a4ea8b5d86c79908e7e8e
Surya Pratap Singh <spsingh2003.sps@gmail.com>
2026-01-28 13:51:19 +0530

    Images are being fetched of the two cards and being displayed

--- index.js
+++ index.js
@@ -8,7 +8,20 @@ function handleClickNewDeck(){
         .then(res=>res.json())
         .then(data=>{
             deckId = data.deck_id
+            console.log(deckId)
         })
-
 }
 NewDeck.addEventListener("click",handleClickNewDeck);
+
+function handlceClickDraw(){
+    fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`)
+    .then(res=>res.json())
+    .then(data=>{
+        console.log(data)
+        document.getElementById("image-container").innerHTML=`
+        <img src="${data.cards[0].image}"/>
+        <img src="${data.cards[1].image}"/>
+        `
+    })
+}
+Draw.addEventListener("click",handlceClickDraw);
